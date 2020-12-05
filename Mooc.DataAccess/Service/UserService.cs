407457e6790d2e692d55543a799cb8cc68b1f78a
John Sun <sunjahoo@naver.com>
2020-12-05 15:25:02 +1100

    Add View pages
    delete
    edit
    modified details

--- Mooc.DataAccess/Service/UserService.cs
+++ Mooc.DataAccess/Service/UserService.cs
@@ -2,8 +2,10 @@
 using Mooc.DataAccess.Context;
 using Mooc.DataAccess.Entities;
 using Mooc.Models.Dtos.User;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.Validation;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -21,9 +23,28 @@ namespace Mooc.DataAccess.Service
 
         public bool Add(CreateOrUpdateUserDto createOrUpdateUserDto)
         {
-            var user = Mapper.Map<User>(createOrUpdateUserDto);
-            this._db.Users.Add(user);
-            return this._db.SaveChanges() > 0;
+            try
+            {
+                var user = Mapper.Map<User>(createOrUpdateUserDto);
+                this._db.Users.Add(user);
+                return this._db.SaveChanges() > 0;
+            }
+
+            catch (DbEntityValidationException e)
+            {
+                foreach (var eve in e.EntityValidationErrors)
+                {
+                    Console.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
+                        eve.Entry.Entity.GetType().Name, eve.Entry.State);
+                    foreach (var ve in eve.ValidationErrors)
+                    {
+                        Console.WriteLine("- Property: \"{0}\", Error: \"{1}\"",
+                            ve.PropertyName, ve.ErrorMessage);
+                    }
+                }
+                throw;
+            }
+
         }
 
         public List<UserDto> GetList()
@@ -34,11 +55,37 @@ namespace Mooc.DataAccess.Service
 
         public async Task<UserDto> GetUser(int id)
         {
-            var user =await _db.Users.FirstOrDefaultAsync(p => p.Id == id);
-            if (user==null)
+            var user = await _db.Users.FirstOrDefaultAsync(p => p.Id == id);
+            if (user == null)
                 return null;
             return Mapper.Map<UserDto>(user);
         }
-       
+
+        //Edit
+        public async Task<CreateOrUpdateUserDto> GetEditUser(int id)
+        {
+            var user = await _db.Users.FirstOrDefaultAsync(p => p.Id == id);
+
+            if (user == null)
+                return null;
+
+            return Mapper.Map<CreateOrUpdateUserDto>(user);
+
+        }
+
+        //Update Edit items
+        public bool Update(CreateOrUpdateUserDto updateUser)
+        {
+            var user = Mapper.Map<User>(updateUser);
+            this._db.Users.Add(user);
+            return this._db.SaveChanges() > 0;
+        }
+
+        public bool Delete(CreateOrUpdateUserDto deleteUser)
+        {
+            var user = Mapper.Map<User>(deleteUser);
+            this._db.Users.Remove(user);
+            return this._db.SaveChanges() > 0;
+        }
     }
 }
