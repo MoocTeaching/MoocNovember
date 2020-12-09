77c76fa71b78cd32b942be906a57670f08fc2c98
jianglili2015 <jianglili2015@gmail.com>
2020-12-10 01:28:51 +1000

    Add helper classes and jquery ajax

--- Mooc.Web/Areas/Admin/Controllers/LoginController.cs
+++ Mooc.Web/Areas/Admin/Controllers/LoginController.cs
@@ -1,6 +1,10 @@
-﻿using Mooc.DataAccess.Dtos.User;
+﻿using Mooc.DataAccess.Context;
+using Mooc.DataAccess.Dtos.User;
+using Mooc.DataAccess.Entities;
+using Mooc.Utils;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,19 +13,66 @@ namespace Mooc.Web.Areas.Admin.Controllers
 {
     public class LoginController : Controller
     {
-        // GET: Admin/Login
+        private readonly DataContext _dataContext;
+
+        public LoginController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+
         public ActionResult Index()
+        {
+            return View("Login");
+        }
+
+
+        public ActionResult Login()
         {
             return View();
         }
 
-        public ActionResult Login(UserDto user )
+        [HttpPost]
+        public JsonResult loginPost(string username, string password)
         {
-            TempData["ErrorMesssage"] = "用户名或者密码错误";
-            return RedirectToAction("Index");
+            User user = _dataContext.Users.FirstOrDefault(m => m.UserName == username);
 
-            //return Redirect("/Home/Index");
+            if (user != null)
+            {
+                string pwd = MD5Help.MD5Encoding(password, ConfigurationManager.AppSettings["sKey"].ToString());
+                if (user.PassWord == pwd)
+                {
+                    //Response.Cookies.Add(new HttpCookie("username")
+                    //{
+                    //    Value = user.UserName,
+                    //    Expires = DateTime.Now.AddDays(7)
+                    //});
+
+                    CookieHelper.SetCookie("username", user.UserName, DateTime.Now.AddDays(7));
+                    CookieHelper.SetCookie("userid", user.Id.ToString(), DateTime.Now.AddDays(7));
+                    return Json(new { code = 0 });
+
+                }
+                return Json(new { code = 1, msg = "密码错误" });
+            }
+            return Json(new { code = 1, msg = "错误" });
+
         }
+
+
+
+
+        public ActionResult DeleteCookie()
+        {
+            CookieHelper.DeleteCookie("username");
+            CookieHelper.DeleteCookie("userid");
+            //if (Request.Cookies["username"] != null)
+            //{
+            //    Response.Cookies["username"].Expires = DateTime.Now.AddDays(-1);
+            //}
+            return Redirect("~/Admin/Login/Index");
+        }
+
     }
 
 
